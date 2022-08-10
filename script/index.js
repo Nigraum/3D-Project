9c351fa2ca7b4a9056573729d3bb8a212352a05c
Nigraum <yan.corintima@hotmail.com>
2022-08-10 04:34:41 -0300

    Create toggle function

--- script/index.js
+++ script/index.js
@@ -2,7 +2,12 @@ function imgSlider(anything) {
   document.querySelector('.drink1').src = anything;
 };
 
-function changeCircleColor(color){
+function changeCircleColor(color) {
   const circle = document.querySelector('.circle');
   circle.style.background = color;
+};
+
+function toggle() {
+  var menuToggle = document.querySelector('.toggle');
+  menuToggle.classList.toggle('active')
 }
